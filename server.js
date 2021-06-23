e2ae7bee7427d7032044bdffc2e0a50571bf1c19
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-06-23 19:23:59 -0400

    have the file structure and some coding started now

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.use(express.json());
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI)
